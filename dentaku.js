1f3520f1212dfb728d196785a47d0d742efa3133
Ivan <iyatsko117@gmail.com>
2024-04-25 17:14:30 +0900

    足し、引き、かけ、割り算のボタンが効きます。

--- dentaku.js
+++ dentaku.js
@@ -3,19 +3,34 @@
 //maximum digits in entry
 const MAX_DIGITS = 12;
 
-let calc_screen_number = 0;
-let decimal_power = 0;
+//additional operand is on the left side of a binary operator
+let additional_operand = null;
+let additional_operand_decimal_power = null;
+
+//binary operator is typical algebraic operator
+let binary_operator = null;
+const binary_operator_list = ["ADD", "SUBTRACT", "MULTIPLY", "DIVIDE"];
+
+//unary operator is for the main operand only
+let unary_operator = null;
+const unary_operator_list = ["SQUARE_ROOT", "PERCENT"];
+
+//main operand is what we see on the calculator screen
+let main_operand = 0;
+let main_operand_decimal_power = 0;
+
+//entry modifiers
 let digits = 0;
 let decimal_just_placed = false;
 
 //Update the number on the display. Useful after input.
-function updateCalcScreen() {
+function updateMainOperand() {
   //toFixed helps round out floating point inaccuracies to the user
-  let out_string = calc_screen_number.toFixed(Math.abs(decimal_power));
+  let out_string = main_operand.toFixed(Math.abs(main_operand_decimal_power));
   if (decimal_just_placed) {
     out_string += ".";
   }
-  document.getElementById("calc_screen").innerText = out_string;
+  document.getElementById("main_operand").innerText = out_string;
 }
 
 //append digit to the number on display, if it can fit
@@ -23,31 +38,48 @@ function appendDigit(num) {
   if (digits >= MAX_DIGITS) {
     return;
   }
-  if (decimal_power == 0 && !decimal_just_placed) {
-    calc_screen_number *= 10;
-    calc_screen_number += num;
-  } else if (decimal_power < 0 || decimal_just_placed) {
+  //make the number to append negative if the overall sign is negative
+  //this is because we use addition. subtraction makes logic harder
+  if (main_operand < 0) {
+    num *= -1;
+  }
+  if (main_operand_decimal_power == 0 && !decimal_just_placed) {
+    main_operand *= 10;
+    main_operand += num;
+  } else if (main_operand_decimal_power < 0 || decimal_just_placed) {
     decimal_just_placed = false;
-    decimal_power--;
-    num = num * Math.pow(10, decimal_power);
-    calc_screen_number += num;
+    main_operand_decimal_power--;
+    num = num * Math.pow(10, main_operand_decimal_power);
+    main_operand += num;
   }
   digits++;
-  updateCalcScreen();
+  updateMainOperand();
 }
 
 function clearEntry() {
-  calc_screen_number = 0;
-  decimal_power = 0;
+  main_operand = 0;
+  main_operand_decimal_power = 0;
   digits = 0;
-  updateCalcScreen();
+  updateMainOperand();
+}
+
+function clearAll() {
+  additional_operand = null;
+  additional_operand_decimal_power = null;
+  binary_operator = null;
+  clearEntry();
 }
 
 function handleDecimalButtonPress() {
-  if (decimal_power == 0 && digits < MAX_DIGITS) {
+  if (main_operand_decimal_power == 0 && digits < MAX_DIGITS) {
     decimal_just_placed = true;
-    updateCalcScreen();
+    updateMainOperand();
   }
+}
+
+function handleChangeSign() {
+  main_operand *= -1;
+  updateMainOperand();
 }
 
 //Handle press of a number button
@@ -59,4 +91,63 @@ function handleNumberButtonPress(num) {
   }
   num = Math.trunc(num);
   appendDigit(num);
+}
+
+function handleBinaryOperator(operator) {
+  //only take operators that are defined
+  if (binary_operator_list.includes(operator)) {
+    if (binary_operator == null && additional_operand == null) {
+      additional_operand = main_operand;
+      additional_operand_decimal_power = main_operand_decimal_power;
+      binary_operator = operator;
+      clearEntry();
+    } else if (main_operand == 0) {
+      binary_operator = operator;
+    } else {
+      //TODO: enqueue the result of the calculation into the additional operand buffer
+      sendCalculation();
+    }
+  }
+  //otherwise throw error and stop execution
+  else {
+    throw ValueError();
+  }
+}
+
+function handleUnaryOperator(operator) {
+  //only take operators that are defined
+  if (unary_operator_list.includes(operator)) {
+    unary_operator = operator;
+    sendCalculation();
+  } else {
+    throw ValueError();
+  }
+}
+
+function sendCalculation() {
+  let data = {};
+  if (additional_operand != null) {
+    data.additional_operand = additional_operand;
+  }
+  if (additional_operand_decimal_power != null) {
+    data.additional_operand_decimal_power = additional_operand_decimal_power;
+  }
+  if (binary_operator != null) {
+    data.binary_operator = binary_operator;
+  }
+  data.main_operand = main_operand;
+  data.main_operand_decimal_power = main_operand_decimal_power;
+  if (unary_operator != null) {
+    data.unary_operator = unary_operator;
+  }
+  fakeSendCalculation(data);
+  unary_operator = null;
+  return;
+}
+
+//placeholder until we do actual network stuff
+function fakeSendCalculation(data) {
+  console.log(data);
+  clearAll();
+  document.getElementById("main_operand").innerText = "faked send";
 }
